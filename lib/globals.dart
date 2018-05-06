3173d1f22ecb87e2d6a36adb4b391eb6350dd5e4
Jason C.H <ctrysbita@outlook.com>
2018-05-06 21:57:40 +0800

    Add flex to empty error page

--- lib/globals.dart
+++ lib/globals.dart
@@ -58,32 +58,35 @@ class EmptyErrorPage extends StatelessWidget {
       : this.onRefresh = onRefresh ?? (() {});
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Card(
-          child: FlatButton(
+  Widget build(BuildContext context) => Card(
+        margin: EdgeInsets.all(20.0),
+        child: FlatButton(
             onPressed: onRefresh,
-            child: Column(
+            child: Flex(
+              direction: Axis.horizontal,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(
-                  Icons.hourglass_empty,
-                  size: 50.0,
-                  color: Theme.of(context).errorColor,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.hourglass_empty,
+                      size: 50.0,
+                      color: Theme.of(context).errorColor,
+                    ),
+                    Padding(padding: EdgeInsets.all(10.0)),
+                    Text(
+                      "Oh! Nothing is here!\nPlease refresh or come later.",
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(padding: EdgeInsets.all(10.0)),
+                    Text(
+                      "噢！这里什么也没有！\n请刷新或稍后再来。",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
                 ),
-                Padding(padding: EdgeInsets.all(10.0)),
-                Text(
-                  "Oh! Nothing is here!\nPlease refresh or come later.",
-                  textAlign: TextAlign.center,
-                ),
-                Padding(padding: EdgeInsets.all(10.0)),
-                Text(
-                  "噢！这里什么也没有！\n请刷新或稍后再来。",
-                  textAlign: TextAlign.center,
-                )
               ],
-            ),
-          ),
-        ),
+            )),
       );
 }
