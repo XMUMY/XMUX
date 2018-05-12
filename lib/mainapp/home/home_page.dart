0b1046d0d90fb84031589178b14693b9f9773b6b
Jason C.H <ctrysbita@outlook.com>
2018-05-12 16:52:41 +0800

    Add homepage refresher

--- lib/mainapp/home/home_page.dart
+++ lib/mainapp/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:xmux/globals.dart';
 import 'package:xmux/mainapp/home/announcements.dart';
 import 'package:xmux/mainapp/home/home_slider.dart';
+import 'package:xmux/mainapp/home/homepage_handler.dart';
 import 'package:xmux/redux/actions.dart';
 import 'package:xmux/redux/state.dart';
 import 'package:xmux/translations/translation.dart';
@@ -20,21 +21,27 @@ class HomePage extends StatelessWidget {
           ),
           title: Text(MainLocalizations.of(context).get("Home")),
         ),
-        body: ListView(
-          children: <Widget>[
-            // Slider
-            Container(
-              child: HomeSlider(),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 18 * 9,
-            ),
+        body: RefreshIndicator(
+          onRefresh: () async {
+            await HomePageHandler.updateNews(context: context);
+            await HomePageHandler.updateAnnouncements(context: context);
+          },
+          child: ListView(
+            children: <Widget>[
+              // Slider
+              Container(
+                child: HomeSlider(),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width / 18 * 9,
+              ),
 
-            // Announcements (if have)
-            StoreConnector<MainAppState, List>(
-              converter: (store) => store.state.uiState.announcements,
-              builder: (_, announcements) => HomeAnnouncements(announcements),
-            ),
-          ],
+              // Announcements (if have)
+              StoreConnector<MainAppState, List>(
+                converter: (store) => store.state.uiState.announcements,
+                builder: (_, announcements) => HomeAnnouncements(announcements),
+              ),
+            ],
+          ),
         ),
       );
 }
