e9d09c1b54e13d6544a9684e5bb1e650818d1429
Jason C.H <ctrysbita@outlook.com>
2019-09-11 14:05:20 +0800

    [maintenance] Add refresh

--- lib/mainapp/campus/maintenance/my_complaint.dart
+++ lib/mainapp/campus/maintenance/my_complaint.dart
@@ -2,7 +2,6 @@ part of 'maintenance.dart';
 
 class MyComplaintsPage extends StatefulWidget {
   final Maintenance maintenance;
-  final List<MyRequest> myComplaints = [];
 
   MyComplaintsPage(this.maintenance);
 
@@ -11,44 +10,52 @@ class MyComplaintsPage extends StatefulWidget {
 }
 
 class _MyComplaintsPageState extends State<MyComplaintsPage> {
+  List<MyRequest> myComplaints;
+
+  Future<Null> _handleUpdate() async {
+    var myRequests = await widget.maintenance.myRequests;
+    if (mounted) setState(() => myComplaints = myRequests);
+  }
+
   @override
   void initState() {
-    if (widget.myComplaints.isEmpty)
-      widget.maintenance.myRequests.then((requests) => mounted
-          ? setState(() => widget.myComplaints.addAll(requests))
-          : null);
+    _handleUpdate();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: widget.myComplaints.length,
-      itemBuilder: (context, index) {
-        var req = widget.myComplaints[index];
-        return Card(
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '${req.title}',
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-                Text(
-                  '${req.id} ${DateFormat.yMMMd().format(req.date)} '
-                  '${req.usage} ${req.category}',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                Divider(height: 8),
-                Text(req.answer)
-              ],
+    if (myComplaints == null) return Center(child: CircularProgressIndicator());
+    return RefreshIndicator(
+      onRefresh: _handleUpdate,
+      child: ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: myComplaints.length,
+        itemBuilder: (context, index) {
+          var req = myComplaints[index];
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    '${req.title}',
+                    style: Theme.of(context).textTheme.subhead,
+                  ),
+                  Text(
+                    '${req.id} ${DateFormat.yMMMd().format(req.date)} '
+                    '${req.usage} ${req.category}',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  Divider(height: 8),
+                  Text(req.answer)
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
