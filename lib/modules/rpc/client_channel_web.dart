bcfd3dbc28a66f3c2a73541f5ea6b627c32dc4bd
Jason C.H <ctrysbita@outlook.com>
2021-05-29 00:07:25 +0800

    Remove old RPC client

--- lib/modules/rpc/client_channel_web.dart
+++
@@ -1,6 +0,0 @@
-import 'package:grpc/grpc_web.dart' as grpc;
-import 'package:grpc/service_api.dart';
-
-/// Create client channel using gRPC web API.
-ClientChannel createClientChannel(String address) =>
-    grpc.GrpcWebClientChannel.xhr(Uri.parse(address));
